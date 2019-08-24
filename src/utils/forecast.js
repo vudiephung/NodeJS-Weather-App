38a6562905ce11189a84b9d27c7ebe0f91902b80
Hung Vu <hungvudiep@gmail.com>
2019-08-24 14:40:16 +0700

    add new degree features

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability * 100 + '% chance of rain. UV index: ' + body.currently.uvIndex)
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out with a low of ' + body.daily.data[0].temperatureLow + ' degrees and high of ' + body.daily.data[0].temperatureHigh + ' degrees. There is a ' + body.currently.precipProbability * 100 + '% chance of rain. UV index: ' + body.currently.uvIndex)
         }
     })
 }
